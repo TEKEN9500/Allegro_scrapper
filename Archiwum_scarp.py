c2d1d998ea21c2632e38329287fb679f15950129
TEKEN9500 <49673150+TEKEN9500@users.noreply.github.com>
2019-04-16 17:34:14 +0200

    Update Archiwum_scarp.py

--- Archiwum_scarp.py
+++ Archiwum_scarp.py
@@ -4,12 +4,15 @@ import re
 import pandas as pd
 import numpy as np
 
+
+# ----THE SAME AS ALLEGRO SCRAPPER BUT ADJUSTED TO ARCHIVE OF ALLEGRO, DIFFERENT CLASSES, LESS PARAMETERS, INFROMATION IN OTHER PLACES-----
+
 p = re.compile('/oferta/.+?(?=")')
 global_df = pd.DataFrame()
 counter = 0
 for page in range(1, 160):
     response = requests.get(
-        'https://archiwum.allegro.pl/kategoria/obraz-i-grafika-karty-graficzne-257236?p={}'.format(page))
+        'https://archiwum.allegro.pl/kategoria/obraz-i-grafika-karty-graficzne-257236?p={}'.format(page)) #other link
     soup = BeautifulSoup(response.text, 'html.parser')
     posts = soup.find_all(class_='_8d855a8')
     print('page: {}'.format(page))
@@ -32,15 +35,15 @@ for page in range(1, 160):
             parameters = offer.find_all(class_='e70c44ee')
             values = offer.find_all(class_='_2036b558')
             price = str(offer.find(class_="asi-offer__price m-price m-price--primary"))
-            price = re.search('(\d+ ?\d*|Nie było ofert kupna)', price).group(1)
-            price_offered = re.search('fee8042">(\d\d? ?\d*)', str(post)).group(1)
+            price = re.search('(\d+ ?\d*|Nie było ofert kupna)', price).group(1) # find price or 'No purchase offers'
+            price_offered = re.search('fee8042">(\d\d? ?\d*)', str(post)).group(1) #find price proposed by seller(not final price)
             re.search('(\d\d? ?\d*)',str('1 700')).group(1)
-            df['Nazwa'] = name.replace("Kup podobny do ", "")
-            df['Cena kupiona'] = price.replace(" ", "")
-            df['Cena zaoferowana'] = price_offered.replace(" ", "")
+            df['Nazwa'] = name.replace("Kup podobny do ", "") # in archive name there is "Buy simmiliar to" before actual name, delete it
+            df['Cena kupiona'] = price.replace(" ", "")    # price bought(final) - remove spaces
+            df['Cena zaoferowana'] = price_offered.replace(" ", "")   # price offered by seller - remove spaces
             df['Link'] = link
             df['offer_id'] = offer_id
-            df['Stan:'] = stan
+            df['Stan:'] = stan # condition: new/used
 
             df.drop(0, axis=1, inplace=True)
             for i in range(0, len(parameters)):
@@ -50,13 +53,20 @@ for page in range(1, 160):
         counter += 1
         print('post: {}'.format(counter))
         print(link)
+        
+        
 
-global_df['Kupione']=0
+        
+"""#in archve there is no information about how many units were bought because these are single offers, -----------------------------
+so if the price bought exists at all instead of " No offers" it means it was bought once and here I will define that"""
+
+global_df['Kupione']=0    
 global_df.loc[global_df['Cena kupiona'] != 'Niebyłoofertkupna', 'Kupione' ] = 1
 global_df.loc[global_df['Cena kupiona'] == 'Niebyłoofertkupna','Cena kupiona'] = 'Brak'
 
-global_df['Link'][93]
-global_df.to_csv('pierwsze_archiwum.csv')
+
+
+"""----------------FROM NOW ON BASICALLY EVERYTHING IS ANALOGICAL TO THE ALLEGRO.PY---------------------------- """
 
 global_df['Cena zaoferowana'] = global_df['Cena zaoferowana'].astype(int)
 global_df = global_df[global_df['Cena zaoferowana']>400]
@@ -149,16 +159,12 @@ global_df.reset_index(drop=True, inplace=True)
 global_df['Model'] = global_df['Model']+global_df['Nr model']
 global_df.drop('Nr model', axis=1, inplace=True, )
 
-global_df['Cena'] = 0
-global_df['Cena'] = global_df['Cena zaoferowana']
+global_df['Cena'] = global_df['Cena zaoferowana'] #it turns out that actual price was always price offered so thats how system works
 
-global_df = pd.read_csv('archiwum_1.csv')
-#global_df.rename(columns={"Stan": "Stan:"}, inplace=True)
 global_df.to_csv('archiwum_1.csv')
 
 """
 global_df_dummy = pd.get_dummies(global_df)
-global_df_dummy.to_csv('global_df_dummy_2.csv')
-
+global_df_dummy.to_csv('global_df_dummy_2.csv')  if necessary for modeling
 global_df = pd.read_csv('global_df_2.csv')
 """
