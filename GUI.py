13db87283ac41d587c5d71629db6cee8d36bdcc5
TEKEN9500 <49673150+TEKEN9500@users.noreply.github.com>
2019-04-16 18:01:55 +0200

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -3,15 +3,20 @@ from qtpy import QtCore, QtGui, uic, QtWidgets
 import weighted
 import pandas as pd
 
+"""-------------------THIS IS MAIN APPLICATION WITH INTERFACE-------------------"""
+
+
+# -------READ CSV FILES CREATED IN ALLEGRO.PY AND ARCHIWUM_SCARP.PY-------------
 global_df = pd.read_csv('global_df_2.csv')
 archiwum = pd.read_csv('archiwum_1.csv')
 global_df = global_df.append(archiwum, sort=False)
 
 
-qtCreatorFile = "123.ui"  # Enter file here.
+qtCreatorFile = "123.ui"  # load interface design created in QT DESIGNER
 
+
+# -----------------Template for creating py apps out of QT DESIGNER ui files - not coded by me ---------------------
 Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
-
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
@@ -20,15 +25,17 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.setupUi(self)
         self.buttonSUBMIT.clicked.connect(self.display_stats)
 
+        
+        
+""" ----------------- THE CORE OF THE PROJECT THAT TAKES MODEL FROM INPUT AND CREATES STATISTICS FOR IT -------------------------"""
+
     def display_stats(self):
-        series = self.inputSERIES.text().upper()
+        series = self.inputSERIES.text().upper()   # take inputs from GUI
         model = self.inputMODEL.text().upper()
         full = series + model
 
-        ########################
-
         loc_df = global_df.loc[global_df['Model'] == full]
-        used = loc_df[loc_df['Stan:'] == 'Używany']
+        used = loc_df[loc_df['Stan:'] == 'Używany']       # create separate dataframes for used and new models
         new = loc_df[loc_df['Stan:'] == 'Nowy']
 
         del loc_df
@@ -36,21 +43,24 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         used.reset_index(drop=True, inplace=True)
         final_output = ''
 
-        ###
-        s_iloczyn = 0
-        s_wag = 0
+        """ -------- Calculate variables needed for weighted mean, if there is no such model give such information --------
+         Save all of the responses (what should be displayed) to the final_output variable. - instead of print in case of using
+         terminal """
+        
+        s_iloczyn = 0 # sum of multiplications for weighted mean
+        s_wag = 0     # sum of weights (number of bought units - ( column: Kupione:)
         if new.empty:
-            final_output += "\n Nowe:       -----------------------------"
-            final_output += '\n\n Brak takiego nowego modelu'
+            final_output += "\n New:       -----------------------------"
+            final_output += '\n\n No such new model'
 
         else:
 
             if new['Kupione'].sum() == 0:
 
-                final_output += "\n \nNowe:       -----------------------------"
-                final_output += "\n \nŚrednia ważona : Brak - 0 sprzedanych "
-                final_output += "\n \nMediana ważona : Brak - 0 sprzedanych"
-                final_output += "\n \nStatystyki: \n "
+                final_output += "\n \nNew:       -----------------------------"
+                final_output += "\n \nWeighted mean : Null - 0 sold "
+                final_output += "\n \nWeighted median : Null - 0 sold "
+                final_output += "\n \nStatistics: \n "
                 final_output += new["Cena"].describe()
 
             else:
@@ -60,26 +70,26 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
                     s_wag += kupione
                 weighted_average = s_iloczyn / s_wag
 
-                final_output += "\n \nNowe:       -----------------------------"
-                final_output += "\n \nŚrednia ważona : " + str(weighted_average)
-                final_output += "\n \nMediana ważona : " + str(weighted.median(new['Cena'], new['Kupione']))
-                final_output += "\n \nStatystyki: \n\n "
+                final_output += "\n \nNew:       -----------------------------"
+                final_output += "\n \nWeighted mean : " + str(weighted_average)
+                final_output += "\n \nWeighted median : " + str(weighted.median(new['Cena'], new['Kupione']))
+                final_output += "\n \nStatistics: \n\n "
                 final_output += str(new["Cena"].describe())
 
         ###
         s_iloczyn = 0
         s_wag = 0
         if used.empty:
-            final_output += "\n\n Używane:       --------------------------"
-            final_output += '\n\n Brak takiego używanego modelu'
+            final_output += "\n\n Used:       --------------------------"
+            final_output += '\n\n No such used model'
         else:
 
             if used['Kupione'].sum() == 0:
 
-                final_output += "\n\n Używane:       --------------------------"
-                final_output += "\n\n Średnia ważona : Brak - 0 sprzedanych "
-                final_output += "\n\n Mediana ważona : Brak - 0 sprzedanych"
-                final_output += "\n\n Statystyki: \n\n "
+                final_output += "\n \nUsed:       -----------------------------"
+                final_output += "\n \nWeighted mean : Null - 0 sold "
+                final_output += "\n \nWeighted median : Null - 0 sold "
+                final_output += "\n \nStatistics: \n "
                 final_output += used["Cena"].describe()
             else:
                 for i in range(0, len(used)):
@@ -88,16 +98,16 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
                     s_wag += kupione
                 weighted_average = s_iloczyn / s_wag
 
-                final_output += "\n\n Używane:       -----------------------------"
-                final_output += "\n\n Średnia ważona : " + str(weighted_average)
-                final_output += "\n\n Mediana ważona : " + str(weighted.median(used['Cena'], used['Kupione']))
-                final_output += "\n\n Statystyki: \n\n "
+                final_output += "\n\n Used:       -----------------------------"
+                final_output += "\n\n Weighted mean : " + str(weighted_average)
+                final_output += "\n\n Weighted median : " + str(weighted.median(used['Cena'], used['Kupione']))
+                final_output += "\n\n Statistics: \n\n "
                 final_output += used["Cena"].describe().to_string()
 
-        self.textBrowser.setText(final_output)
-        print(final_output)
+        self.textBrowser.setText(final_output) #display output in GUI display window (text browser class)
+        
 
-
+# end part of template - allows application to last
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     window = MainWindow()
